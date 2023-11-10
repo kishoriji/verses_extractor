eeb17f0ccfb31ca99c70ed83ae9a5ec640abbe31
Akash <a.chandrakar@matterlabs.dev>
2023-11-10 22:17:10 +0400

    added offset

--- slokas_audio_extractor.py
+++ slokas_audio_extractor.py
@@ -2,12 +2,13 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 
-def convert_time(str_time):
+def convert_time(str_time, offset):
     parts = str_time.split(':')
     m, s = map(int, parts[:2])
     d = int(parts[2]) if len(parts) > 2 else 0
     time_in_seconds = m * 60 + s + d / 10
-    return time_in_seconds + 15
+    return time_in_seconds + offset
+    # return time_in_seconds + 15
 
 
 def split_audio(filepath, start_time, end_time):
@@ -21,11 +22,13 @@ def split_audio(filepath, start_time, end_time):
 def extract_audio(txt_filepath, audio_filepath, export_path):
     slokas = []
     with open(txt_filepath, 'r', encoding='utf-8') as file:
+        offset_line = file.readline()
+        offset = float(offset_line.strip())
         for line in file.readlines():
             sloka_verse_no, start_time_str, end_time_str = [item.strip() for item in line.strip().split(',')]
-            start_time, end_time = convert_time(start_time_str), convert_time(end_time_str)
+            start_time, end_time = convert_time(start_time_str, offset), convert_time(end_time_str, offset)
             slokas.append((sloka_verse_no, start_time, end_time))
-    sloka = slokas[4]
+    sloka = slokas[11]
     sloka_verse_no, start_time, end_time = sloka
     print(sloka_verse_no, start_time, end_time)
     output_file = f"{export_path}/{sloka_verse_no}.mp3"
@@ -35,7 +38,8 @@ def extract_audio(txt_filepath, audio_filepath, export_path):
 
 
 if __name__ == '__main__':
-    txt_filepath = 'slokas_location_in_lecture/Brahm Jeev Maya Kya Hai 15 [qCekFCGy4Ro].txt'
-    audio_filepath = "/Users/akashmatterlabs/Downloads/translate/Brahm Jeev Maya Kya Hai 15 [qCekFCGy4Ro].mp3"
-    export_path = 'slokas/Brahm Jeev Maya Kya Hai 15 [qCekFCGy4Ro]'
+    name = 'Brahm Jeev Maya Kya Hai 16 [ei6Z8RaB2N0]'
+    txt_filepath = f'slokas_location_in_lecture/{name}.txt'
+    audio_filepath = f"/Users/akashmatterlabs/Downloads/translate/{name}.mp3"
+    export_path = f'slokas/{name}'
     extract_audio(txt_filepath, audio_filepath, export_path)
