7144a08e96f9663e34140b68d03a8bc8461a4751
kishoriji <kishoriji@gmail.com>
2023-12-09 16:51:01 +0400

    update data

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,8 +1,7 @@
 import os
 
 
-def main():
-    name = 'Shruti Siddhant Saar/Shruti_Siddhant_Saar_2_12417'
+def create_playlist(name):
     folder_path = os.path.abspath(f'slokas/{name}')  # get absolute path of the directory
     playlist_path = f"{folder_path}/playlist.m3u"
 
@@ -16,5 +15,16 @@ def main():
                     file.write(name + '\n')
 
 
+def create_playlists():
+    for top_root, top_dirs, _ in os.walk('slokas_location_in_lecture'):
+        for top_dir in top_dirs:
+            for root, _, files in os.walk(os.path.join(top_root, top_dir)):
+                for file in files:
+                    name = file.split('.')[0]
+                    path = os.path.join(top_dir, name)
+                    create_playlist(path)
+
+
 if __name__ == '__main__':
-    main()
+    # create_playlist('Shruti Siddhant Saar/Shruti_Siddhant_Saar_2_12417')
+    create_playlists()
